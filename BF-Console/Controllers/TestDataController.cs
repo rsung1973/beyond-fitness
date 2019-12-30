6df2eb997049b99d0a7656362955f68143e829b6
ralph <rsung1973@hotmail.com>
2019-12-31 00:28:21 +0800

    [2019-12-31]:for data query test

---
+++ BF-Console/Controllers/TestDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+using CommonLib.DataAccess;
+using CommonLib.MvcExtension;
+using Newtonsoft.Json;
+using Utility;
+using WebHome.Controllers;
+using WebHome.Helper;
+using WebHome.Helper.BusinessOperation;
+using WebHome.Models.DataEntity;
+using WebHome.Models.Locale;
+using WebHome.Models.Timeline;
+using WebHome.Models.ViewModel;
+using WebHome.Properties;
+using WebHome.Security.Authorization;
+
+namespace WebHome.Controllers
+{
+    public class TestDataController : SampleController<UserProfile>
+    {
+        // GET: TestData
+        public ActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
